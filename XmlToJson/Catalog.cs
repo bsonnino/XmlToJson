2134891ac55186d56858020602d5fd06742d3d1e
sonnino@revolution.com.br <Bruno>
2021-01-08 07:40:59 -0500

    Add project files.

---
+++ XmlToJson/Catalog.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XmlToJson
+{
+
+    // NOTE: Generated code may require at least .NET Framework 4.5 or .NET Core/Standard 2.0.
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class catalog
+    {
+
+        private catalogBook[] bookField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("book")]
+        public catalogBook[] book
+        {
+            get
+            {
+                return this.bookField;
+            }
+            set
+            {
+                this.bookField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.SerializableAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class catalogBook
+    {
+
+        private string authorField;
+
+        private string titleField;
+
+        private string genreField;
+
+        private decimal priceField;
+
+        private System.DateTime publish_dateField;
+
+        private string descriptionField;
+
+        private string idField;
+
+        /// <remarks/>
+        public string author
+        {
+            get
+            {
+                return this.authorField;
+            }
+            set
+            {
+                this.authorField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string title
+        {
+            get
+            {
+                return this.titleField;
+            }
+            set
+            {
+                this.titleField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string genre
+        {
+            get
+            {
+                return this.genreField;
+            }
+            set
+            {
+                this.genreField = value;
+            }
+        }
+
+        /// <remarks/>
+        public decimal price
+        {
+            get
+            {
+                return this.priceField;
+            }
+            set
+            {
+                this.priceField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute(DataType = "date")]
+        public System.DateTime publish_date
+        {
+            get
+            {
+                return this.publish_dateField;
+            }
+            set
+            {
+                this.publish_dateField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string description
+        {
+            get
+            {
+                return this.descriptionField;
+            }
+            set
+            {
+                this.descriptionField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string id
+        {
+            get
+            {
+                return this.idField;
+            }
+            set
+            {
+                this.idField = value;
+            }
+        }
+    }
+
+
+}
---
+++ XmlToJson/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace XmlToJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var serializer = new XmlSerializer(typeof(catalog));
+
+            //using var reader = new StreamReader("books.xml");
+            //var catalog = (catalog)serializer.Deserialize(reader);
+            //reader.Close();
+
+            //var options = new JsonSerializerOptions { WriteIndented = true };
+            //var jsonCatalog = JsonSerializer.Serialize(catalog, options);
+            //File.WriteAllText("books.json", jsonCatalog);
+            //var serializer = new XmlSerializer(typeof(catalog));
+
+            var serializer = new XmlSerializer(typeof(object));
+            using var reader = new StreamReader("books.xml");
+            var catalog = serializer.Deserialize(reader);
+            reader.Close();
+
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonCatalog = JsonSerializer.Serialize(catalog, options);
+            File.WriteAllText("books.json", jsonCatalog);
+        }
+    }
+
+}
